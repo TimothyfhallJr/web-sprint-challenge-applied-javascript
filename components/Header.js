40005cf8d0c03dbcfa2e7dc84f6abc5dc9a9d069
unknown <TimothyfhallJr@gmail.com>
2020-08-14 20:38:32 -0400

    trying again

--- components/Header.js
+++ components/Header.js
@@ -11,35 +11,61 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
+
 function Header() {
-    // creating
 
-    const headerDiv = document.createElement('div');
-    const date = document.createElement('span');
-    const title = document.createElement('h1');
-    const temp = document.createElement('span');
+  // creating 
+//    creating elements 
 
-    // appending
+const headerDiv = document.createElement('div');
+const headerSpan1 = document.createElement('span');
+const headerTitle = document.createElement('h1');
+const headerSpan2 = document.createElement('span');
 
-    headerDiv.appendChild('date');
-    headerDiv.appendChild('date');
-    headerDiv.appendChild('temp');
+  const headerDiv = document.querySelector('div');
+  const headerSpan1 = document.querySelector('span');
+  const headerTitle = document.querySelector('h1');
+  const headerSpan2 = document.querySelector('span');
 
-    // class lists
+  // pieces appending
+// appending elements 
 
-    headerDiv.classList.add('header');
-    date.classList.add('date');
-    temp.classList.add('temp');
+  headerDiv.appendChild(headerSpan1);
+  headerDiv.appendChild(headerTitle);
+  headerDiv.appendChild(headerSpan2);
+headerDiv.appendChild(headerSpan1);
+headerDiv.appendChild(headerTitle);
+headerDiv.appendChild(headerSpan2);
 
-    // Content Update 
+  // classLists 
+//class Listing
 
-    date.textContent('March 28, 2020');
-    title.textContent('Lambda Times');
-    temp.textContent('98&deg');
+  headerDiv.classList.add('header');
+  deaderSpan1.classList.add('date');
+  headerSpan2.classList.add('temp');
 
-    return headerDiv;
+  // Content Update 
+
+  headerSpan1.textContent('SMARCH 28, 2019');
+  headerTitle.textContent('Lambda Times');
+  headerSpan2.textContent('98°');
+
+  return headerDiv;
+headerDiv.classList.add('header');
+headerSpan1.classList.add('date');
+headerSpan2.classList.add('temp');
+
+// textContent
+
+headerSpan1.textContent = 'MARCH 28, 2019';
+headerTitle.textContent = 'Lambda Times';
+headerSpan2.textContent = '98°';
+
+return headerDiv;
 };
 
-const headerContainer = document.querySelector('.header-container');
+let headerContainer = document.querySelector('.header-container');
 
-headerContainer.append(Header());
+headerContainer.append(Header()); 
+headerContent = document.querySelector('.header-container');
+headerContent.appendChild(Header());
